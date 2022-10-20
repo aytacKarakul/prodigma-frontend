dfebd6cfacc7c83178d5ebf7cc7972754d0d2cb6
aytac_k <aytac@agencyist.com>
2022-10-20 16:13:18 +0300

    slider added

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import './index.scss';
 import Partials from 'Partials';
 import Components from 'Components';
 import LoginTabs from './templates/components/tab/index';
+import OurSerivesSwiper from './templates/widgets/our-services';
 
 new Partials();
 new Components();
+new OurSerivesSwiper();
 new LoginTabs();
 
 import 'Images/google-icon.svg';
---
+++ src/templates/widgets/our-services/index.js
@@ -0,0 +1,44 @@
+import Swiper, { Navigation, Scrollbar } from 'swiper';
+
+import 'Images/slider-bg.png';
+import 'Images/3d-tasarim.png';
+import 'Images/cnc-isleme.png';
+import 'Images/silikon-kaplama.png';
+import 'Images/enjeksiyon-kaliplama.png';
+
+class OurSerivesSwiper {
+    constructor(){
+        const swiper = new Swiper('.swiper', {
+            modules: [Navigation, Scrollbar],
+            slidesPerView: 1.3,
+            spaceBetween: 20,
+            //loop: true,
+
+            breakpoints: {
+                640: {
+                    slidesPerView: 2.2,
+                    spaceBetween: 10,
+                },
+                768: {
+                    slidesPerView: 3.2,
+                    spaceBetween: 10,
+                },
+                1024: {
+                    slidesPerView: 4.2,
+                    spaceBetween: 20,
+                },
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            scrollbar: {
+                el: '.swiper-scrollbar',
+                draggable: true,
+            },
+        });
+        return swiper;
+    }
+}
+
+export default OurSerivesSwiper;
