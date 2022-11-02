55109f34e8ac268b04c0ab3020724a94e836368f
aytac_k <aytac@agencyist.com>
2022-11-02 16:45:21 +0300

    blog banner some update

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import MainSwiperSlider from './templates/widgets/home-page-main-slider';
 import QuadrapleBanner from './templates/widgets/quadraple-banner';
 import SwiperParalax from './templates/widgets/paralax-banner';
 import OurSerivesSwiper from './templates/widgets/our-services';
+import HomePageBlogBanner from './templates/widgets/prodigma-plus-banner';
 
 new Partials();
 new Components();
@@ -15,6 +16,7 @@ new MainSwiperSlider();
 new QuadrapleBanner();
 new SwiperParalax();
 new OurSerivesSwiper();
+new HomePageBlogBanner();
 new LoginTabs();
 
 import 'Images/google-icon.svg';
---
+++ src/templates/widgets/prodigma-plus-banner/index.js
@@ -0,0 +1,10 @@
+import 'Images/img_1.jpg';
+import 'Images/img_2.jpg';
+import 'Images/img_3.jpg';
+import 'Images/img_4.jpg';
+import 'Images/img_5.jpg';
+import 'Images/img_6.jpg';
+class HomePageBlogBanner {
+    constructor(){}
+}
+export default HomePageBlogBanner;
