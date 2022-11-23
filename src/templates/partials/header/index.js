eb9cc75c03f535d67e5cbb553febb0798a1b3e59
aytac_k <aytac@agencyist.com>
2022-11-23 19:11:43 +0300

    header mini member added

--- src/templates/partials/header/index.js
+++ src/templates/partials/header/index.js
@@ -3,6 +3,7 @@ import 'Images/logo.svg';
 class Header{
     constructor(){
         this.header = document.querySelector('.site-header');
+        this.memberMenuHover = document.querySelector('.site-header-login-success'); 
 
         this.initHeader();
     }
@@ -13,6 +14,12 @@ class Header{
         });
         this.header?.addEventListener('mouseleave', (event) => {
             event.target.classList.remove('site-header-anim');
+        });
+        this.memberMenuHover?.addEventListener('mouseenter', (event) => {
+            event.target.classList.add('animate');
+        });
+        this.memberMenuHover?.addEventListener('mouseleave', (event) => {
+            event.target.classList.remove('animate');
         });
     }
 }
