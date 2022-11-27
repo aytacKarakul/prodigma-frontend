9d78b328af12ff19df1637aa6d803e4b9954a2b3
aytac_k <aytac@agencyist.com>
2022-11-28 00:48:52 +0300

    main slider revize img

--- src/templates/widgets/home-page-main-slider/index.js
+++ src/templates/widgets/home-page-main-slider/index.js
@@ -1,17 +1,23 @@
-import Swiper, { Navigation, Pagination, EffectFade } from 'swiper';
+import Swiper, { Navigation, Pagination, EffectFade, Autoplay } from 'swiper';
 
 import 'Images/aselsan.png';
 import 'Images/roketsan.png';
 import 'Images/turkish-technic.png';
-import 'Images/mercedes-benz.png';
+import 'Images/slider_1x2.1.jpg';
+import 'Images/slider_1x2.jpg';
+import 'Images/slider_2x2.1.jpg';
+import 'Images/slider_2x2.jpg';
 
 class MainSwiperSlider {
     constructor(){
         const swiperMain = new Swiper('.swiper-main', {
-            modules: [EffectFade, Navigation, Pagination],
+            modules: [EffectFade, Navigation, Pagination, Autoplay],
             effect: 'fade',
             spaceBetween: 0,
             slidesPerView: 'auto',
+            autoplay: {
+                delay: 7000,
+            },
             navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
