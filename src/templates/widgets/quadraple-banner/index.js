4dfb1e801e754163670d692c52cc545a7a55464a
aytac_k <aytac@agencyist.com>
2022-11-28 12:55:55 +0300

    quadraple banner gsap added

--- src/templates/widgets/quadraple-banner/index.js
+++ src/templates/widgets/quadraple-banner/index.js
@@ -1,6 +1,33 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 import 'Images/how-the-works-prodigma.png';
 import 'Images/animation-vectorel.svg';
 
 export const quadrapleBanner = () => {
-    console.log('sdsd');
+    
+    gsap.registerPlugin(ScrollTrigger);
+    const gsapElement = document.querySelectorAll('.quadruple-banner-body-right ul li');
+
+    if(gsapElement){
+        gsapElement.forEach((item) => {
+            gsap.to(item, {
+                y:-500,
+                duration: 8,
+                scrollTrigger: {
+                    trigger: item,
+                    start: "bottom 0%",
+                    end: "top 10%",
+                    scrub: 4,
+                    toggleActions: "restart none none none",
+                    pin: true,
+                    markers: {
+                        startColor: "purple",
+                        endColor: "fuchsia",
+                        fontSize: "1rem"
+                    }
+                }
+            });
+        })
+    }
 }
