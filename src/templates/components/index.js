b6e1012e12ddc48fce62fc80f992c12d6d89d1e2
aytac_k <aytac@agencyist.com>
2022-12-05 08:56:07 +0300

    adresleri popup tamamlandı

--- src/templates/components/index.js
+++ src/templates/components/index.js
@@ -1,12 +1,14 @@
 import Inputs from './web-component/inputs';
 import Accordion from './accordion';
 import { MemberDropdown } from './member/navigation';
+import { MemberAddressPopup } from './member/address-list';
 
 class Components {
     constructor(){
         new Inputs();
         new Accordion();
         MemberDropdown();
+        MemberAddressPopup();
     }
 }
 
---
+++ src/templates/components/member/address-list/index.js
@@ -0,0 +1,24 @@
+export const MemberAddressPopup = () => {
+    const selectPopupWrapper = document.querySelector(".member-address-add-popup");
+    const addressPopuopVail = document.querySelector(".vail-address-add");
+    const selectBtn = document.querySelector("#js-member-page-new-address-add-popup");
+    const closeBtn = document.querySelector(".member-address-add-popup-title .icon");
+
+    if(selectBtn && closeBtn){
+        selectBtn.addEventListener("click", () => {
+            document.body.style.overflow = "hidden";
+            addressPopuopVail.classList.add("open");
+            selectPopupWrapper.classList.add("open");
+        });
+        closeBtn.addEventListener("click", () => {
+            document.body.style.removeProperty("overflow");
+            selectPopupWrapper.classList.remove("open");
+            addressPopuopVail.classList.remove("open");
+        });
+        addressPopuopVail.addEventListener("click", () => {
+            document.body.style.removeProperty("overflow");
+            selectPopupWrapper.classList.remove("open");
+            addressPopuopVail.classList.remove("open");
+        });
+    }
+};
