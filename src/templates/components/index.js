8a2a1fe1b09fe92e0b5faf6bfbecf34e91275773
aytac_k <aytac@agencyist.com>
2022-12-05 08:57:40 +0300

    popup index js duzeltme

--- src/templates/components/index.js
+++ src/templates/components/index.js
@@ -1,14 +1,12 @@
 import Inputs from './web-component/inputs';
 import Accordion from './accordion';
 import { MemberDropdown } from './member/navigation';
-import { MemberAddressPopup } from './member/address-list';
 
 class Components {
     constructor(){
         new Inputs();
         new Accordion();
         MemberDropdown();
-        MemberAddressPopup();
     }
 }
 
