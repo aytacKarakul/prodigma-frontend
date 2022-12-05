f6e6a82c37513d57bee1f16e4f93297c79799721
aytac_k <aytac@agencyist.com>
2022-12-05 10:21:56 +0300

    conflict solution

--- src/templates/components/index.js
+++ src/templates/components/index.js
@@ -1,12 +1,16 @@
 import Inputs from './web-component/inputs';
 import Accordion from './accordion';
 import { MemberDropdown } from './member/navigation';
+import { OrderDetail } from './member/order-detail-list';
+import { MemberAddressPopup } from './member/address-list';
 
 class Components {
     constructor(){
         new Inputs();
         new Accordion();
         MemberDropdown();
+        OrderDetail();
+        MemberAddressPopup();
     }
 }
 
