10dad0772ae99f4230d5e0289cf7691b4017e870
aytacKarakul <aytac.karakul@gmail.com>
2023-01-09 15:45:46 +0300

    gsap

--- src/templates/widgets/quadraple-banner/index.js
+++ src/templates/widgets/quadraple-banner/index.js
@@ -6,23 +6,4 @@ import "Images/animation-vectorel.svg";
 
 export const quadrapleBanner = () => {
   gsap.registerPlugin(ScrollTrigger);
-  const gsapElement = document.querySelectorAll(
-    ".quadruple-banner-body-right ul li"
-  );
-
-  if (gsapElement) {
-    gsapElement.forEach((el) => {
-      let tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: el,
-          start: "bottom 70%",
-          end: "bottom center",
-          markers: true,
-          toggleActions: "play none reverse none",
-          scrub: 1,
-        },
-      });
-      tl.to(el, { opacity: 0, yPercent: -10 });
-    });
-  }
 };
