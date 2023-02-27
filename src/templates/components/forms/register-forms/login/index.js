86e46aee72683d62ffa48dbb7e89470bb8b97150
aytacKarakul <aytac.karakul@gmail.com>
2023-02-27 14:01:03 +0300

    user login update

--- src/templates/components/forms/register-forms/login/index.js
+++ src/templates/components/forms/register-forms/login/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { apitoken } from "../../../../auth/authentication";
-import AuthUser from "../../../../utils/AuthUser";
+//import AuthUser from "../../../../utils/AuthUser";
 
 class AuthUserLogin {
   constructor() {
@@ -21,8 +21,8 @@ class AuthUserLogin {
         ).value;
 
         var data = new FormData(loginForm);
-        data.append("eposta", `${email}`);
-        data.append("sifre", `${password}`);
+        data.append("eposta", email);
+        data.append("sifre", password);
 
         axios
           .post(`${process.env.API_KEY}` + "/User/login", data, {
@@ -32,13 +32,18 @@ class AuthUserLogin {
             },
           })
           .then((res) => {
-            let getLoginHash = res.data.login_hash;
-            if (getLoginHash != undefined) {
-              localStorage.setItem("login_hash", getLoginHash);
-              new AuthUser(res.data.ad, res.data.soyad);
-              window.location.href = "/";
+            if (res.data.error) {
+              if (res.data.error.eposta && res.data.error.sifre) {
+                alert("Lütfen E-Posta ve Şifrenizi kontrol ediniz!");
+              } else if (res.data.error.eposta) {
+                alert("Lütfen E-Posta adresinizi giriniz!");
+              } else if (res.data.error.sifre) {
+                alert("Lütfen şifrenizi giriniz!");
+              }
+              console.log(res);
             } else {
-              console.log("alert");
+              localStorage.setItem("login_hash", JSON.stringify(res.data));
+              window.location.href = "/";
             }
           })
           .catch((err) => console.log(err));
@@ -47,13 +52,23 @@ class AuthUserLogin {
   }
   userUIControl() {
     const locHash = localStorage.getItem("login_hash");
+    const userName = document.querySelector(".site-header-login-member");
+    const userSurname = document.querySelector(
+      ".site-header-member-menu-header .bttm .tt2"
+    );
+    const obj = JSON.parse(locHash);
+
     if (locHash) {
+      userName.innerHTML = obj.ad + " " + obj.soyad;
+      userSurname.innerHTML = obj.ad + " " + obj.soyad;
       document
         .querySelector(".site-header-login-success")
         .parentElement.removeAttribute("hidden");
       document.querySelector(".site-header-logoff").classList.add("hidden");
     } else {
       document.querySelector(".site-header-logoff").classList.remove("hidden");
+      userName.innerHTML = "";
+      userSurname.innerHTML = "";
     }
   }
   logout() {
