5d5709b7d6985f1110d925ccf4e386b41e3cc41a
aytacKarakul <aytac.karakul@gmail.com>
2023-02-28 00:43:56 +0300

    header language #1

--- src/templates/partials/header/index.js
+++ src/templates/partials/header/index.js
@@ -4,8 +4,10 @@ class Header {
   constructor() {
     this.header = document.querySelector(".site-header");
     this.memberMenuHover = document.querySelector(".site-header-login-success");
+    this.headerLangBtns = document.querySelectorAll(".site-header-lang button");
 
     this.initHeader();
+    this.langChangeFunc();
   }
 
   initHeader() {
@@ -21,6 +23,38 @@ class Header {
     this.memberMenuHover?.addEventListener("mouseleave", (event) => {
       event.target.classList.remove("animate");
     });
+    this.headerLangBtns?.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        const btnId = e.target.id;
+        if (e.target.classList.contains("selected")) {
+          e.target.classList.remove("selected");
+        } else {
+          const selectedBtn = document.querySelectorAll(
+            ".site-header-lang button.selected"
+          );
+          selectedBtn.forEach((btn) => {
+            btn.classList.remove("selected");
+          });
+        }
+        e.target.classList.add("selected");
+        localStorage.setItem("lang", btnId);
+
+        // html lang change
+        document.documentElement.lang = btnId;
+      });
+    });
+  }
+
+  langChangeFunc() {
+    if (localStorage.getItem("lang") == "en") {
+      const firstBtn =
+        document.querySelector(".site-header-lang").firstElementChild;
+      firstBtn.classList.add("selected");
+    } else if (localStorage.getItem("lang") == "tr") {
+      const lastBtn =
+        document.querySelector(".site-header-lang").lastElementChild;
+      lastBtn.classList.add("selected");
+    }
   }
 }
 
