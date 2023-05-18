6190710faebd672f2f47e7979fa39a6395785d24
aytacKarakul <aytac.karakul@gmail.com>
2023-05-18 14:49:02 +0300

    webinar update

--- src/templates/widgets/prodigma-plus-page/index.js
+++ src/templates/widgets/prodigma-plus-page/index.js
@@ -5,7 +5,6 @@ import modalPopup from "../../components/web-component/modal/index";
 class ProdigmaPlusPage {
   constructor() {
     this.onLoadBlogLists();
-    //this.onLoadWebinarsLists();
     const containerWrapper = document.querySelector(".pprodigma-plus");
     if (containerWrapper) {
       modalPopup();
@@ -41,9 +40,9 @@ class ProdigmaPlusPage {
                 html.innerHTML += `<div>
                 <a href="${process.env.SITE_DOMAIN}/blog/${element.url}">
                 <picture>
-                  <source media="(max-width: 1024px)" srcset="${process.env.SITE_DOMAIN}${element.mobil_resim}">
-                  <source media="(min-width: 1025px)" srcset="${process.env.SITE_DOMAIN}${element.resim}">
-                  <img src="${process.env.SITE_DOMAIN}${element.mobil_resim}" alt="${element.title}">
+                  <source media="(max-width: 1024px)" srcset="${process.env.SITE_DOMAIN}${element.kapak_mobil_resim}">
+                  <source media="(min-width: 1025px)" srcset="${process.env.SITE_DOMAIN}${element.kapak_resim}">
+                  <img src="${process.env.SITE_DOMAIN}${element.kapak_mobil_resim}" alt="${element.title}">
                 </picture>
                 <div class="btxt">
                   <div class="bt1">${element.title}</div>
@@ -70,28 +69,6 @@ class ProdigmaPlusPage {
               }
             }
         });
-      }
-    }).catch((error) => console.log(error))
-  }
-  async onLoadWebinarsLists(){
-    await axios.get((`${process.env.API_KEY}` + "/webinars/get"), {
-      auth: {
-        username: "prodigma3d",
-        password: `${apitoken}`,
-      },
-    }).then(function (res){
-      if(res.status === 200){
-        const webinars = res.data.filter(item => item.dil === document.documentElement.lang);
-
-        //blog list sorts
-        const webinarList = webinars.sort((a, b) => a.id - b.id);
-        
-        webinarList.forEach((items) => {
-          if(items.durum == 1){
-            console.log(items);
-          }
-        });
-        
       }
     }).catch((error) => console.log(error))
   }
