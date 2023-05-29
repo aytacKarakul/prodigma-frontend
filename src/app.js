1c43b7172c1d0d8724188462c85393d3c114d278
aytacKarakul <aytac.karakul@gmail.com>
2023-05-29 11:40:39 +0300

    commit last updates

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
+import axios from "axios";
 import "Images/favicon.ico";
 import "Theme/base.scss";
 import "./index.scss";
 
-
-import getApiToken from "./templates/auth/authentication";
 import Partials from "Partials";
 import Components from "Components";
 import Widgets from "./templates/widgets";
 import Flatpages from "./templates/flatpages";
-import Basket from "./templates/basket";
+//import Basket from "./templates/basket";
+
+axios
+  .get(`${process.env.API_KEY}` + "/token")
+  .then((res) => {
+    let data = res.data;
+    localStorage.setItem("apitoken", data);
+    return data;
+  })
+  .catch((err) => console.log(err));
 
 class AppJs {
   constructor() {
-    if(localStorage.getItem("apitoken")){
-      getApiToken();
-    }
-    new Partials();
-    new Components();
-    new Widgets();
-    new Flatpages();
-    new Basket();
+    setTimeout(() => {
+      new Partials();
+      new Components();
+      new Widgets();
+      new Flatpages();
+      //new Basket();
+    }, 500);
   }
 }
-
 new AppJs();
